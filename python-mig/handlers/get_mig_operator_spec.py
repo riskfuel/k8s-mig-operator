9968f2d040037c2acb8504df668c672976ce1423
addyvan <avandenhoeve14@ubishops.ca>
2020-08-17 10:49:33 -0400

    updating daemonset pod security policy

--- python-mig/handlers/get_mig_operator_spec.py
+++ python-mig/handlers/get_mig_operator_spec.py
@@ -1,10 +1,16 @@
 from .utils import run_shell_cmd
 import yaml
+import logging
+log = logging.getLogger(__name__)
 
 def get_mig_operator_spec(operator_name : str = "example-mig-operator-auto", operator_ns : str = "default") -> dict:
 
     raw : str = run_shell_cmd(f"kubectl -n {operator_ns} get migoperators {operator_name} -o yaml")
 
-    migoperator = yaml.safe_load(raw)
-    spec = migoperator["spec"]
-    return spec
+    try:
+        migoperator = yaml.safe_load(raw)
+        spec = migoperator["spec"]
+        return spec
+    except Exception as e:
+        log.error(e)
+        return {}
