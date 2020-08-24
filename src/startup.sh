6dbccd3bec93be3c83e7af8ff01f5fe7d7f4292d
addyvan <avandenhoeve14@ubishops.ca>
2020-08-24 11:54:51 -0400

    ansible init scripts not runnign quick fix

--- src/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -z "${ALLOW_NODE_RESET}" ]]; then
-python3 init_ansible.py
-ansible-playbook --become --become-user=root --connection=local ./ansible/init.yml
-ansible-playbook --become --become-user=root -i ./ansible/inventory.yml ./ansible/start-gpu-services.yml
-fi
-python3 index.py
